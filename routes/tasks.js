29bb35fbfb050e4fd2f16a2fd9a74fae3779efec
jonArzelus <kontrabass32@hotmail.com>
2017-04-28 14:38:34 +0200

    aurrerapenak

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mytasklist', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-	res.send('TASK API');
+	//res.send('TASK API');
+	db.tasks.find(function(err, tasks){
+		if(err){
+			res.send(err);
+		}
+		res.json(tasks);
+	});
 });
 
 module.exports = router;
+
+/*
+use mytasklist;
+db.createCollection("tasks");
+var tk = db.getCollection("tasks");
+tk.insertOne({izena:"jon",abizena:"arzelus"})
+tk.insertOne({izena:"mikel",abizena:"ocejo"})
+*/
